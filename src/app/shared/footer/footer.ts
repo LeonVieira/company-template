bbd4f46031eec02d41d9712ea3197ad286b29c1a
Leon Vieira <leonvieira456@gmail.com>
2026-07-20 15:45:31 +0200

    Minor styling updates to header and footer start of using boostrap for styling and form creation for contact us button

--- src/app/shared/footer/footer.ts
+++ src/app/shared/footer/footer.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   imports: [],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
+  encapsulation: ViewEncapsulation.None
 })
 export class Footer {
 
